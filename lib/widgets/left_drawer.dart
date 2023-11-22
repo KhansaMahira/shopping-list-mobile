c5b520a4d46d08fc5cca24b133bb33c6526ccb36
KhansaMahira <khansamahira011@gmail.com>
2023-11-22 13:48:33 +0700

    tutorial9

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/screens/list_product.dart';
+import 'package:shopping_list/screens/pages/list_product.dart';
 import 'package:shopping_list/screens/menu.dart';
 // Impor halaman ShopFormPage jika sudah dibuat
 import 'package:shopping_list/screens/shoplist_form.dart';
